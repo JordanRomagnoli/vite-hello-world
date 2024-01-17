e9e5324ac2114909af48b132500d7b39b18a5b42
Jordan Romagnoli <146195147+JordanRomagnoli@users.noreply.github.com>
2024-01-17 13:30:26 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<script>
+
+/*
+    Importazione componente
+*/
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data() {
+        return { 
+            count: 0 
+        }
+    },
+    /*
+        Dichiarazione dei componenti all'interno dell'oggetto
+    */
+    components: {
+
+        AppHeader
+    },
+}
+</script>
+
+<template>
+    <div>
+        <!--
+            Utilizzo del componente
+        -->
+        <AppHeader/>
+        <h1>
+            Ciao Ragazzi
+        </h1>
+
+        <p>
+            {{ count }}
+        </p>
+
+        <button @click="count++">
+            Incrementa count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+    export default {
+        data() {
+            return { 
+                title: 'Ciao ragazzi' 
+            }
+        },
+    }
+    </script>
+
+<template>
+    <header class="main-title">
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+    h1{
+        color: aquamarine;
+        text-align: center;
+    }
+</style>
